c05893432453c7f364cbdf5e43a4dccea7b92fbe
Ethan Zhang <ezsland05@gmail.com>
2023-05-27 19:59:09 -0700

    modified edited .gitignore

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,5 +1,5 @@
 require("bufferline").setup{}
 
-vim.api.nvim_set_keymap('n', 'bt', [[BufferLineCycleNext<CR>]], {})
-vim.api.nvim_set_keymap('n', 'bT', [[BufferLineCyclePrev<CR>]], {})
-vim.api.nvim_set_keymap('n', '<leader>bd', [[:bd<CR>]], {})
+vim.api.nvim_set_keymap('n', 'gt', [[<Cmd>BufferLineCycleNext<CR>]], {})
+vim.api.nvim_set_keymap('n', 'gT', [[<Cmd>BufferLineCyclePrev<CR>]], {})
+vim.api.nvim_set_keymap('n', '<leader>bd', [[<Cmd>bd<CR>]], {})
