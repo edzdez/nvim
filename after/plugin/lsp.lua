88bbb876c18cbb41e8342fe95d8eca563aff28b4
Ethan Zhang <ezsland05@gmail.com>
2023-08-08 16:08:44 -0700

    updates

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -7,7 +7,7 @@ require("lsp-colors").setup({
 
 local cmp = require('cmp')
 local cmp_sources = {
-    {name = 'path'}, {name = 'nvim_lsp'}, {name = 'luasnip', keyword_length = 2}
+    { name = 'path' }, { name = 'nvim_lsp' }, { name = 'luasnip', keyword_length = 2 }
 }
 require('luasnip.loaders.from_vscode').lazy_load()
 
@@ -21,7 +21,7 @@ cmp.setup({
             else
                 fallback()
             end
-        end, {"i", "s"}),
+        end, { "i", "s" }),
 
         ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
@@ -29,11 +29,29 @@ cmp.setup({
             else
                 fallback()
             end
-        end, {"i", "s"})
+        end, { "i", "s" }),
+
+        ["<CR>"] = cmp.mapping({
+            i = function(fallback)
+                if cmp.visible() and cmp.get_active_entry() then
+                    cmp.confirm({
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = false
+                    })
+                else
+                    fallback()
+                end
+            end,
+            s = cmp.mapping.confirm({ select = true }),
+            c = cmp.mapping.confirm({
+                behavior = cmp.ConfirmBehavior.Replace,
+                select = true
+            })
+        })
     },
     sources = cmp_sources,
-    completion = {completeopt = 'menu,menuone,noinsert,noselect,preview'},
-    experimental = {ghost_text = true}
+    completion = { completeopt = 'menu,menuone,noinsert,noselect,preview' },
+    experimental = { ghost_text = true }
 })
 
 -- lsp.set_sign_icons({
@@ -44,25 +62,25 @@ cmp.setup({
 -- })
 
 function attach(client, bufnr)
-    local opts = {buffer = bufnr, remap = false}
+    local opts = { buffer = bufnr, remap = false }
 
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
 
-    vim.keymap.set("n", "<leader>K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
 
     vim.keymap.set("i", "<C-k>", function() vim.lsp.buf.signature_help() end,
-                   opts)
+        opts)
     vim.keymap.set("n", "<leader>vd",
-                   function() vim.diagnostic.open_float() end, opts)
+        function() vim.diagnostic.open_float() end, opts)
 
     vim.keymap.set("n", "<leader>vws",
-                   function() vim.lsp.buf.workleader_symbol() end, opts)
+        function() vim.lsp.buf.workleader_symbol() end, opts)
     vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
     vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder,
-                   bufopts)
+        bufopts)
 
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
@@ -72,18 +90,18 @@ function attach(client, bufnr)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
 end
 
-require"lsp_signature".on_attach({
+require "lsp_signature".on_attach({
     bind = true, -- This is mandatory, otherwise border config won't get registered.
-    handler_opts = {border = "rounded"},
+    handler_opts = { border = "rounded" },
     toggle_key = "<C-K>"
 }, bufnr)
 
 require("mason").setup()
 require("mason-lspconfig").setup()
 local lspconfig = require('lspconfig')
-lspconfig.ocamllsp.setup({on_attach = attach})
-lspconfig.lua_ls.setup({on_attach = attach})
-lspconfig.texlab.setup({on_attach = attach})
+lspconfig.ocamllsp.setup({ on_attach = attach })
+lspconfig.lua_ls.setup({ on_attach = attach })
+lspconfig.texlab.setup({ on_attach = attach })
 
 local rust_tools = require('rust-tools')
 rust_tools.setup({
@@ -96,26 +114,26 @@ rust_tools.setup({
             highlight = "Comment"
         }
     },
-    server = {on_attach = attach, standalone = true}
+    server = { on_attach = attach, standalone = true }
 })
 
-require('clangd_extensions').setup {server = {on_attach = attach}}
+require('clangd_extensions').setup { server = { on_attach = attach } }
 
-vim.diagnostic.config({virtual_text = true})
+vim.diagnostic.config({ virtual_text = true })
 
 require('lean').setup {
-    abbreviations = {builtin = true},
-    lsp = {on_attach = attach},
-    lsp3 = {on_attach = attach},
+    abbreviations = { builtin = true },
+    lsp = { on_attach = attach },
+    lsp3 = { on_attach = attach },
     mappings = true
 }
 
 -- vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 
-local kopts = {noremap = true, silent = true}
+local kopts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap('n', ' cx',
-                        [[<Cmd>:lua vim.diagnostic.open_float(nil, {focus = false})<CR>]],
-                        kopts)
+    [[<Cmd>:lua vim.diagnostic.open_float(nil, {focus = false})<CR>]],
+    kopts)
 
 require("mason-null-ls").setup({
     ensure_installed = {
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,129 +1,140 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') ..
+                             '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({
+            'git', 'clone', '--depth', '1',
+            'https://github.com/wbthomason/packer.nvim', install_path
+        })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  -- Base 16 themes
-  use {'RRethy/nvim-base16'}
-  -- use {'chriskempson/base16-vim'}
+    -- Base 16 themes
+    use {'RRethy/nvim-base16'}
+    -- use {'chriskempson/base16-vim'}
 
-  -- Gruvbox
-  use { "ellisonleao/gruvbox.nvim" }
-  -- use "sainnhe/gruvbox-material"
+    -- Gruvbox
+    use {"ellisonleao/gruvbox.nvim"}
+    -- use "sainnhe/gruvbox-material"
 
-  -- treesitter
-  use(
-      'nvim-treesitter/nvim-treesitter',
-      {run = ':TSUpdate'}
-  )
-  use {'nvim-treesitter/playground'}
+    -- treesitter
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use {'nvim-treesitter/playground'}
 
-  -- telescope
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
-  --tabs
-  use {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'}
-
-  -- undotree
-  use {'mbbill/undotree'}
-
-  -- vimfugitive
-  use {'tpope/vim-fugitive'}
-
-  -- lsp
-  use {'neovim/nvim-lspconfig'}
-  use{'williamboman/mason.nvim'}
-  use{'williamboman/mason-lspconfig.nvim'}
-  use{'hrsh7th/nvim-cmp'}
-  use{'hrsh7th/cmp-nvim-lsp'}
-  use{'hrsh7th/cmp-path'}
-  use{'L3MON4D3/LuaSnip'}
-  use{'saadparwaiz1/cmp_luasnip'}
-  use{ "rafamadriz/friendly-snippets"}
-  use {'folke/lsp-colors.nvim'}
-  use { "ray-x/lsp_signature.nvim" }
-  use {'simrat39/rust-tools.nvim'}
-  use {'simrat39/inlay-hints.nvim'}
-  use {'p00f/clangd_extensions.nvim'}
-  use {'hood/popui.nvim'}
-  use {'Julian/lean.nvim'}
-
-  -- LaTeX
-  use 'lervag/vimtex'
-
-  -- error lens
-  use { "folke/trouble.nvim", requires = "kyazdani42/nvim-web-devicons" }
-
-  -- Auto format tools
-  use({ "sbdchd/neoformat", cmd = { "Neoformat" } })
-  use("jay-babu/mason-null-ls.nvim")
-  use("jose-elias-alvarez/null-ls.nvim")
-
-  -- show and trim trailing whitespaces
-  use {'jdhao/whitespace.nvim', event = 'VimEnter'}
-
-  -- colors!
-  use { 'norcalli/nvim-colorizer.lua' }
-
-  -- which-key
-  use {
-    "folke/which-key.nvim",
-    config = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-      require("which-key").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end
-  }
-
-  -- The missing auto-completion for cmdline!
-  use({"gelguy/wilder.nvim", opt = true, setup = [[vim.cmd('packadd wilder.nvim')]]})
-
-  -- Plugin to manipulate character pairs quickly
-  use({"machakann/vim-sandwich", event = "VimEnter"})
-
-  -- lualine
-  use { 'nvim-lualine/lualine.nvim', requires = { 'nvim-tree/nvim-web-devicons', opt = true } }
-
-  -- auto-close pairs
-  use({"Raimondi/delimitMate", event = "InsertEnter"})
-
-  -- vimwiki
-  use {'vimwiki/vimwiki'}
-
-  -- indentline
-  -- use {'Yggdroot/indentLine'}
-  use "lukas-reineke/indent-blankline.nvim"
-
-  -- tree
-  use {
-    'nvim-tree/nvim-tree.lua',
-    requires = {
-      'nvim-tree/nvim-web-devicons', -- optional
+    -- telescope
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = {{'nvim-lua/plenary.nvim'}}
     }
-  }
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+    -- tabs
+    use {
+        'akinsho/bufferline.nvim',
+        tag = "*",
+        requires = 'nvim-tree/nvim-web-devicons'
+    }
+
+    -- undotree
+    use {'mbbill/undotree'}
+
+    -- vimfugitive
+    use {'tpope/vim-fugitive'}
+
+    -- lsp
+    use {'neovim/nvim-lspconfig'}
+    use {'williamboman/mason.nvim'}
+    use {'williamboman/mason-lspconfig.nvim'}
+    use {'hrsh7th/nvim-cmp'}
+    use {'hrsh7th/cmp-nvim-lsp'}
+    use {'hrsh7th/cmp-path'}
+    use {'L3MON4D3/LuaSnip'}
+    use {'saadparwaiz1/cmp_luasnip'}
+    use {"rafamadriz/friendly-snippets"}
+    use {'folke/lsp-colors.nvim'}
+    use {"ray-x/lsp_signature.nvim"}
+    use {'simrat39/rust-tools.nvim'}
+    use {'simrat39/inlay-hints.nvim'}
+    use {'p00f/clangd_extensions.nvim'}
+    use {'hood/popui.nvim'}
+    use {'Julian/lean.nvim'}
+    use 'mfussenegger/nvim-dap'
+
+    -- LaTeX
+    use 'lervag/vimtex'
+
+    -- error lens
+    use {"folke/trouble.nvim", requires = "kyazdani42/nvim-web-devicons"}
+
+    -- Auto format tools
+    use({"sbdchd/neoformat", cmd = {"Neoformat"}})
+    use("jay-babu/mason-null-ls.nvim")
+    use("jose-elias-alvarez/null-ls.nvim")
+
+    -- show and trim trailing whitespaces
+    use {'jdhao/whitespace.nvim', event = 'VimEnter'}
+
+    -- colors!
+    use {'norcalli/nvim-colorizer.lua'}
+
+    -- which-key
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
+
+    -- The missing auto-completion for cmdline!
+    use({
+        "gelguy/wilder.nvim",
+        opt = true,
+        setup = [[vim.cmd('packadd wilder.nvim')]]
+    })
+
+    -- Plugin to manipulate character pairs quickly
+    use({"machakann/vim-sandwich", event = "VimEnter"})
+
+    -- lualine
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = {'nvim-tree/nvim-web-devicons', opt = true}
+    }
+
+    -- auto-close pairs
+    use({"Raimondi/delimitMate", event = "InsertEnter"})
+
+    -- vimwiki
+    use {'vimwiki/vimwiki'}
+
+    -- indentline
+    -- use {'Yggdroot/indentLine'}
+    use "lukas-reineke/indent-blankline.nvim"
+
+    -- tree
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons' -- optional
+        }
+    }
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then require('packer').sync() end
 end)
