c2a711ca605771f19b8cab6f9dc19561b7cf1ccf
Ethan Zhang <ezsland05@gmail.com>
2023-08-20 21:38:29 -0500

    lsp updates

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,6 +1,6 @@
 local cmp = require('cmp')
 local cmp_sources = {
-    { name = 'path' }, { name = 'nvim_lsp' }, { name = 'luasnip', keyword_length = 2 }
+    {name = 'path'}, {name = 'nvim_lsp'}, {name = 'luasnip', keyword_length = 2}
 }
 require('luasnip.loaders.from_vscode').lazy_load()
 
@@ -14,7 +14,7 @@ cmp.setup({
             else
                 fallback()
             end
-        end, { "i", "s" }),
+        end, {"i", "s"}),
 
         ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
@@ -22,7 +22,7 @@ cmp.setup({
             else
                 fallback()
             end
-        end, { "i", "s" }),
+        end, {"i", "s"}),
 
         ["<CR>"] = cmp.mapping({
             i = function(fallback)
@@ -35,7 +35,7 @@ cmp.setup({
                     fallback()
                 end
             end,
-            s = cmp.mapping.confirm({ select = true }),
+            s = cmp.mapping.confirm({select = true}),
             c = cmp.mapping.confirm({
                 behavior = cmp.ConfirmBehavior.Replace,
                 select = true
@@ -43,8 +43,8 @@ cmp.setup({
         })
     },
     sources = cmp_sources,
-    completion = { completeopt = 'menu,menuone,noinsert,noselect,preview' },
-    experimental = { ghost_text = true }
+    completion = {completeopt = 'menu,menuone,noinsert,noselect,preview'},
+    experimental = {ghost_text = true}
 })
 
 -- lsp.set_sign_icons({
@@ -55,7 +55,7 @@ cmp.setup({
 -- })
 
 function attach(client, bufnr)
-    local bufopts = { buffer = bufnr, remap = false }
+    local bufopts = {buffer = bufnr, remap = false}
 
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
@@ -65,15 +65,15 @@ function attach(client, bufnr)
     vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
 
     vim.keymap.set("i", "<C-k>", function() vim.lsp.buf.signature_help() end,
-        opts)
+                   opts)
     vim.keymap.set("n", "<leader>vd",
-        function() vim.diagnostic.open_float() end, opts)
+                   function() vim.diagnostic.open_float() end, opts)
 
     vim.keymap.set("n", "<leader>vws",
-        function() vim.lsp.buf.workleader_symbol() end, opts)
+                   function() vim.lsp.buf.workleader_symbol() end, opts)
     vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
     vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder,
-        bufopts)
+                   bufopts)
 
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
@@ -82,27 +82,30 @@ function attach(client, bufnr)
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
 
-    vim.keymap.set('n', '<leader>bp', require 'dap'.toggle_breakpoint, bufopts)
-    vim.keymap.set('n', '<leader>sO', require 'dap'.step_over, bufopts)
-    vim.keymap.set('n', '<leader>si', require 'dap'.step_into, bufopts)
-    vim.keymap.set('n', '<leader>so', require 'dap'.step_out, bufopts)
-    vim.keymap.set('n', '<leader>sr', require 'dap'.continue, bufopts)
-    vim.keymap.set('n', '<leader>ro', require 'dap'.repl.open, bufopts)
+    vim.keymap.set('n', '<leader>bp', require'dap'.toggle_breakpoint, bufopts)
+    vim.keymap.set('n', '<leader>sO', require'dap'.step_over, bufopts)
+    vim.keymap.set('n', '<leader>si', require'dap'.step_into, bufopts)
+    vim.keymap.set('n', '<leader>so', require'dap'.step_out, bufopts)
+    vim.keymap.set('n', '<leader>sr', require'dap'.continue, bufopts)
+    vim.keymap.set('n', '<leader>ro', require'dap'.repl.open, bufopts)
 end
 
-require "lsp_signature".on_attach({
+require"lsp_signature".on_attach({
     bind = true, -- This is mandatory, otherwise border config won't get registered.
-    handler_opts = { border = "rounded" },
+    handler_opts = {border = "rounded"},
     toggle_key = "<C-K>"
 }, bufnr)
 
 require("mason").setup()
 require("mason-lspconfig").setup()
 local lspconfig = require('lspconfig')
-lspconfig.ocamllsp.setup({ on_attach = attach })
-lspconfig.lua_ls.setup({ on_attach = attach })
-lspconfig.texlab.setup({ on_attach = attach })
-lspconfig.dafny.setup({ on_attach = attach })
+lspconfig.ocamllsp.setup({on_attach = attach})
+lspconfig.lua_ls.setup({on_attach = attach})
+lspconfig.texlab.setup({on_attach = attach})
+lspconfig.pyright.setup({on_attach = attach})
+lspconfig.dafny.setup({on_attach = attach})
+lspconfig.gopls.setup({on_attach = attach})
+lspconfig.elixirls.setup({on_attach = attach})
 lspconfig.clangd.setup({
     on_attach = function(client, bufnr)
         attach(client, bufnr)
@@ -122,10 +125,10 @@ rust_tools.setup({
             highlight = "Comment"
         }
     },
-    server = { on_attach = attach, standalone = true }
+    server = {on_attach = attach, standalone = true}
 })
 
-vim.diagnostic.config({ virtual_text = true })
+vim.diagnostic.config({virtual_text = true})
 require("clangd_extensions").setup({
     inlay_hints = {
         inline = vim.fn.has("nvim-0.10") == 1,
@@ -138,7 +141,7 @@ require("clangd_extensions").setup({
         -- not that this may cause  higher CPU usage.
         -- This option is only respected when only_current_line and
         -- autoSetHints both are true.
-        only_current_line_autocmd = { "CursorHold" },
+        only_current_line_autocmd = {"CursorHold"},
         -- whether to show parameter hints with the inlay hints or not
         show_parameter_hints = true,
         -- prefix for parameter hints
@@ -161,18 +164,18 @@ require("clangd_extensions").setup({
 })
 
 require('lean').setup {
-    abbreviations = { builtin = true },
-    lsp = { on_attach = attach },
-    lsp3 = { on_attach = attach },
+    abbreviations = {builtin = true},
+    lsp = {on_attach = attach},
+    lsp3 = {on_attach = attach},
     mappings = true
 }
 
 -- vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 
-local kopts = { noremap = true, silent = true }
+local kopts = {noremap = true, silent = true}
 vim.api.nvim_set_keymap('n', ' cx',
-    [[<Cmd>:lua vim.diagnostic.open_float(nil, {focus = false})<CR>]],
-    kopts)
+                        [[<Cmd>:lua vim.diagnostic.open_float(nil, {focus = false})<CR>]],
+                        kopts)
 
 require("mason-null-ls").setup({
     ensure_installed = {
